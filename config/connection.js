38db3d96bc2542d4def96175d08e6bf7e3aeeaef
Joe Wright <joewrightart@gmail.com>
2018-05-25 12:56:41 -0400

    jawsDB added

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "recipes_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "recipes_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
